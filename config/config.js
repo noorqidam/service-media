d878752c45359df61c872249bf00068ef1f739e2
noorqidam <noorqidam@gmail.com>
2021-10-14 20:57:29 +0700

    update again1234

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOSTNAME, DB_URL } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOSTNAME } = process.env;
 
 module.exports = {
   development: {
@@ -24,7 +24,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    use_env_variable: DB_URL,
+    use_env_variable: DATABASE_URL,
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
